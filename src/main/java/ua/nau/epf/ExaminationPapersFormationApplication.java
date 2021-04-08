946c7c3fef3e52e6547c173a12bd2ac9cf82609f
Maksym Ivanchenko <maksym_ivanchenko@epam.com>
2021-04-08 11:04:24 +0300

    Initial commit

---
+++ src/main/java/ua/nau/epf/ExaminationPapersFormationApplication.java
@@ -0,0 +1,13 @@
+package ua.nau.epf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminationPapersFormationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExaminationPapersFormationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/nau/epf/ExaminationPapersFormationApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.nau.epf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminationPapersFormationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
